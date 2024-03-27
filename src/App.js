436c3bc4e792f997e57e5c7487f1e000227a5620
Woo <jiwoopark727@gmail.com>
2024-03-27 21:29:49 +0900

    버튼에 기능개발 & 리액트 state 변경하는 법

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   let post = '강남 우동 맛집';
   let [title, setTitle] = useState(['남자코트추천', '강남우동맛집','파이썬독학']);
+  let [good, setGood] = useState(0);
 
   return (
     <div className="App">
@@ -16,9 +17,18 @@ function App() {
       </div>
 
       <div className='list'>
-        <h4>{ title[0] }</h4>
+        <span onClick={()=>{
+          let copy = [...title];
+          copy[0] = "여자코트추천";
+          setTitle(copy);
+        }}>🔄</span>
+        
+        <h4>{ title[0] } <span onClick={()=>{setGood(good+1)}}>👍</span>
+         {good} </h4>
         <p>2월 17일 발행</p>
       </div>
+
+
       <div className='list'>
         <h4>{ title[1] }</h4>
         <p>2월 18일 발행</p>
@@ -27,7 +37,7 @@ function App() {
         <h4>{ title[2] }</h4>
         <p>2월 19일 발행</p>
       </div>
-      
+
     </div>
   );
 }
