08a28f7a74970f30eae3dfb0667df63a06f455ba
Woo <jiwoopakr727@gmail.com>
2024-03-27 21:23:17 +0900

    중요한 데이터는 state에 저장

--- src/App.js
+++ src/App.js
@@ -2,18 +2,33 @@
 
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [title, setTitle] = useState(['남자코트추천', '강남우동맛집','파이썬독학']);
 
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4>블로그임</h4>
-        </div>
-        <p>{ post }</p>
-     </div>
+        <h4>React Blog</h4>
+      </div>
+
+      <div className='list'>
+        <h4>{ title[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title[1] }</h4>
+        <p>2월 18일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title[2] }</h4>
+        <p>2월 19일 발행</p>
+      </div>
+      
+    </div>
   );
 }
 
